e82d73c1f564129a8e040b5b7e513ec141c6e672
Aluno <fernandobf.ata@gmail.com>
2024-09-12 21:42:00 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vetores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            Console.WriteLine("Quantas pessoas você quer cadrastras");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            
+            string[] nomes = new string[quantidade];
+            int[] idades = new int[quantidade];
+
+
+            for (int i = 0; i < idades.Length; i++)
+            {
+                Console.Write("Poderia me falar seu NOME: ");
+                nomes[i] = Console.ReadLine();
+           
+                Console.Write("Poderia me falar sua IDADE: ");
+                idades[i] = int.Parse(Console.ReadLine());
+
+            }
+             
+            for (int i = 0; i < nomes.Length; i++)
+            {
+                Console.WriteLine($"{nomes[i]} {idades[i]} ");
+            }    
+              
+        }    
+    }
+}
